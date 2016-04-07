862fce4d97c766c3329dc1eb238dcb15222a6298
massanex <fotome-saito@co.kayac.com>
2016-04-07 13:43:49 +0900

    Ark スケルトン 生成 ほぼ直後状態

---
+++ Jobeet/blib/lib/Jobeet.pm
@@ -0,0 +1,34 @@
+package Jobeet;
+use Ark;
+
+use_model 'Jobeet::Models';
+our $VERSION = '0.01';
+
+__PACKAGE__->meta->make_immutable;
+
+__END__
+
+=head1 NAME
+
+Jobeet -
+
+=head1 SYNOPSIS
+
+use Jobeet;
+
+=head1 DESCRIPTION
+
+Jobeet is
+
+=head1 AUTHOR
+
+saito E<lt>fotome-saito@co.kayac.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
