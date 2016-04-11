b883ea758d2ea90daecaa4b9c727fc3f29b8005e
massanex <fotome-saito@co.kayac.com>
2016-04-11 18:10:11 +0900

    06日目通過

--- Jobeet/lib/Jobeet/Controller/Job.pm
+++ Jobeet/lib/Jobeet/Controller/Job.pm
@@ -14,3 +14,29 @@ sub index :Path :Args(0) {
 
 __PACKAGE__->meta->make_immutable;
 
+# /job/{job_token} （詳細）
+sub show :Path :Args(1) {
+    my ($self, $c, $job_token) = @_;
+}
+
+# /job/create （新規作成）
+sub create :Local {
+    my ($self, $c) = @_;
+}
+
+sub job :Chained('/') :PathPart :CaptureArgs(1) {
+    my ($self, $c, $job_token) = @_;
+    $c->stash->{job_token} = $job_token;
+}
+
+# /job/{job_token}/edit （編集）
+sub edit :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
+# /job/{job_token}/delete （削除）
+sub delete :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
+1;
