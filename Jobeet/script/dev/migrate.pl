0d7857f56cab67aedc229be70c214ac64f0dc30a
massanex <fotome-saito@co.kayac.com>
2016-04-13 12:22:14 +0900

    test

---
+++ Jobeet/script/dev/migrate.pl
@@ -0,0 +1,46 @@
+use v5.18.2;
+use strict;
+use warnings;
+use utf8;
+use lib 'lib';
+
+use Jobeet::Models;
+use Getopt::Long;
+use GitDDL;
+
+GetOptions(
+    \my %options,
+    qw/dry-run/
+);
+
+my $dsn = models('conf')->{database}->[0];
+my $gd = GitDDL->new(
+#    work_tree => './',
+    work_tree => '../',
+#    ddl_file  => './schema.sql',
+    ddl_file  => './Jobeet/sql/schema.sql',
+    dsn       => models('conf')->{database},
+);
+
+my $db_version = '';
+eval {
+    open my $fh, '>', \my $stderr;
+    local *STDERR = $fh;
+    $db_version = $gd->database_version;
+};
+
+if ( !$db_version ) {
+    $gd->deploy;
+    say 'done migrate';
+    exit;
+}
+
+if ($gd->check_version) {
+    say 'Database is already latest';
+} else {
+    say $gd->diff . "\n";
+    if (!$options{'dry-run'}) {
+        $gd->upgrade_database;
+        say 'done migrate';
+    }
+}
